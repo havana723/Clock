7d0b5584f8b053b8c861390682248fe3e6bafb2c
havana723 <71638185+havana723@users.noreply.github.com>
2022-04-07 09:03:10 +0900

    add digit divider

--- src/components/Digit.tsx
+++ src/components/Digit.tsx
@@ -5,11 +5,51 @@ interface Props {
 }
 
 const DigitContainer = styled.div`
-  background: #2f2e2e;
+  background: #242323;
   color: white;
   width: 60px;
   height: 80px;
   border-radius: 10px;
+  position: relative;
+`;
+
+const UpDigitContainer = styled.div`
+  background: #3b3a3a;
+  width: 60px;
+  height: 38px;
+  position: absolute;
+  top: 0;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+
+const DownDigitContainer = styled.div`
+  background: #3b3a3a;
+  width: 60px;
+  height: 38px;
+  position: absolute;
+  bottom: 0;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  overflow: hidden;
+`;
+
+const UpDigit = styled.div`
+  width: 60px;
+  line-height: 80px;
+  text-align: center;
+  font-size: 48px;
+  position: absolute;
+  top: 0;
+`;
+
+const BottomDigit = styled.div`
+  width: 60px;
+  line-height: 80px;
+  text-align: center;
+  font-size: 48px;
+  position: absolute;
+  bottom: 0;
 `;
 
 const Digit: React.FC<Props> = (props) => {
@@ -17,7 +57,14 @@ const Digit: React.FC<Props> = (props) => {
 
   return (
     <>
-      <DigitContainer>{digit}</DigitContainer>
+      <DigitContainer>
+        <UpDigitContainer>
+          <UpDigit>{digit}</UpDigit>
+        </UpDigitContainer>
+        <DownDigitContainer>
+          <BottomDigit>{digit}</BottomDigit>
+        </DownDigitContainer>
+      </DigitContainer>
     </>
   );
 };
